b0be952b40ecb1728f52205b3a9231b657b82841
Andrew <73075564+TetrahydridodinitrogenX@users.noreply.github.com>
2023-10-04 12:14:34 -0400

    Update Potentiometer-LED-Wave.ino

--- Potentiometer-LED-Wave.ino
+++ Potentiometer-LED-Wave.ino
@@ -25,47 +25,24 @@ void setup() {
 void loop() {
 
   int A5Val = analogRead(potPin); // Voltage Across Potentiometer
-  float timeDelay = A5Val * (5.0 / 1023.0); // Converts Read Data To Correct Voltage
-
+  float Voltage = A5Val * (5.0 / 1023.0); // Converts Read Data To Correct Voltage
+  int LEDTime = Voltage * (100.0);
+  
   Serial.print("Current Potentiometer Value: ");
-  Serial.println(timeDelay); // Displays Current Potentiometer Value
+  Serial.println(Voltage); // Displays Current Potentiometer Value
     delay(50);
-
-  for(int i = 0; i < 3; i++){
     
-    // Clockwise LED Rotation
-
-    digitalWrite(2, HIGH); delay(i);
-    digitalWrite(3, HIGH); digitalWrite(2, LOW); delay(timeDelay);
-    digitalWrite(4, HIGH); digitalWrite(3, LOW); delay(timeDelay);
-    digitalWrite(5, HIGH); digitalWrite(4, LOW); delay(timeDelay);
-    digitalWrite(6, HIGH); digitalWrite(5, LOW); delay(timeDelay);
-    digitalWrite(7, HIGH); digitalWrite(6, LOW); delay(timeDelay);
-    digitalWrite(8, HIGH); digitalWrite(7, LOW); delay(timeDelay);
-    digitalWrite(9, HIGH); digitalWrite(8, LOW); delay(timeDelay);
-    digitalWrite(10, HIGH); digitalWrite(9, LOW); delay(timeDelay);
-    digitalWrite(11, HIGH); digitalWrite(10, LOW); delay(timeDelay);
-    digitalWrite(12, HIGH); digitalWrite(11, LOW); delay(timeDelay);
-    digitalWrite(13, HIGH); digitalWrite(12, LOW); delay(timeDelay);
-    digitalWrite(13, LOW); delay(timeDelay);
-  }
-
-  for(int i2 = 0; i2 < 3; i2++){
-
-    // Counter-Clockwise LED Rotation
-
-    digitalWrite(13, HIGH); delay(timeDelay);
-    digitalWrite(12, HIGH); digitalWrite(13, LOW); delay(timeDelay);
-    digitalWrite(11, HIGH); digitalWrite(12, LOW); delay(timeDelay);
-    digitalWrite(10, HIGH); digitalWrite(11, LOW); delay(timeDelay);
-    digitalWrite(9, HIGH); digitalWrite(10, LOW); delay(timeDelay);
-    digitalWrite(8, HIGH); digitalWrite(9, LOW); delay(timeDelay);
-    digitalWrite(7, HIGH); digitalWrite(8, LOW); delay(timeDelay);
-    digitalWrite(6, HIGH); digitalWrite(7, LOW); delay(timeDelay);
-    digitalWrite(5, HIGH); digitalWrite(6, LOW); delay(timeDelay);
-    digitalWrite(4, HIGH); digitalWrite(5, LOW); delay(timeDelay);
-    digitalWrite(3, HIGH); digitalWrite(4, LOW); delay(timeDelay);
-    digitalWrite(2, HIGH); digitalWrite(3, LOW); delay(timeDelay);
-    digitalWrite(2, LOW); delay(timeDelay);
-  }
+    digitalWrite(2, HIGH); delay(LEDTime);
+    digitalWrite(3, HIGH); digitalWrite(2, LOW); delay(LEDTime);
+    digitalWrite(4, HIGH); digitalWrite(3, LOW); delay(LEDTime);
+    digitalWrite(5, HIGH); digitalWrite(4, LOW); delay(LEDTime);
+    digitalWrite(6, HIGH); digitalWrite(5, LOW); delay(LEDTime);
+    digitalWrite(7, HIGH); digitalWrite(6, LOW); delay(LEDTime);
+    digitalWrite(8, HIGH); digitalWrite(7, LOW); delay(LEDTime);
+    digitalWrite(9, HIGH); digitalWrite(8, LOW); delay(LEDTime);
+    digitalWrite(10, HIGH); digitalWrite(9, LOW); delay(LEDTime);
+    digitalWrite(11, HIGH); digitalWrite(10, LOW); delay(LEDTime);
+    digitalWrite(12, HIGH); digitalWrite(11, LOW); delay(LEDTime);
+    digitalWrite(13, HIGH); digitalWrite(12, LOW); delay(LEDTime);
+    digitalWrite(13, LOW); delay(LEDTime);
 }
